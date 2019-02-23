fde5877a0bc22e405b8f19932c3447f57cdd0f37
Fisher Darling <fdarlingco@gmail.com>
2019-02-23 14:12:22 -0700

    Page any key

--- platformer/DialogBox.gd
+++ platformer/DialogBox.gd
@@ -4,14 +4,24 @@ extends RichTextLabel
 # var a = 2
 # var b = "textvar"
 
-var dialog_text = ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"]
+var dialog_text = ["This is the first page", "This is the second page"]
 var num_visible = 0
 var page = 0
 
 func _ready():
 	set_bbcode(dialog_text[page])
 	set_visible_characters(-1)
-	pass
+	set_process_input(true)
+
+
+func _input(event):
+	if event.is_pressed():
+		# print("Pressed")
+		if get_visible_characters() > get_total_character_count():
+			if page < dialog_text.size() - 1:
+				page += 1
+				set_bbcode(dialog_text[page])
+				set_visible_characters(0)
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
