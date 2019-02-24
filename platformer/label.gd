0d186b9db38da1beac1a32ca47c7bde2b92e0165
Jake Vossen <jakevossen@jakevossen.codes>
2019-02-23 21:04:20 -0700

    finished ing abstraction

---
+++ platformer/label.gd
@@ -0,0 +1,33 @@
+extends RichTextLabel
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var t = 0;
+onready var the_name = get_parent().get_name()
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	p()
+	pass
+func p():
+	set_bbcode(the_name + ": " + str(t))
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_plus_pressed():
+	print("plus pressed")
+	t = min(t + 1, 5)
+	p()
+	pass # replace with function body
+
+
+func _on_minus_pressed():
+	print("minus pressed")
+	t = max(t - 1, 0)
+	p()
+	pass # replace with function body
